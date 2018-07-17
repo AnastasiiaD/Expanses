299d260c9a86aaf40376dfd23009f6cad1fedc0e
Anastasiia Dzundza <2007nastia@gmail.com>
2018-07-17 10:28:34 +0300

    first commit

---
+++ my-app/src/components/accordion/accordion.component.js
@@ -0,0 +1,10 @@
+import AccordionController from 'accordion.controller';
+import AcordionTemplate from 'accordion.tpl';
+
+const AccordionComponent = {
+
+  controller: AccordionController,
+  templateUrl: AcordionTemplate,
+}
+
+export default AccordionComponent;
---
+++ my-app/src/components/accordion/accordion.controller.js
@@ -0,0 +1,7 @@
+
+export default class AccordionCtrl {
+
+  constructor() {
+    'ngInject';
+  }
+}
